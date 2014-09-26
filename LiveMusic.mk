46b6273d2bf160691624297722dc7e1330061346
Elias Kesh <elias@kesh.com>
2014-09-25 17:59:44 -0700

    Rewrite Email

--- LiveMusic.mk
+++ LiveMusic.mk
@@ -5,7 +5,7 @@
 ## Debug
 ProjectName            :=LiveMusic
 ConfigurationName      :=Debug
-WorkspacePath          := "/mnt/Personal/home/elias/workspace/LiveMusic"
+WorkspacePath          := "/mnt/Personal/home/elias/workspace/EliasMusic"
 ProjectPath            := "/mnt/Personal/home/elias/workspace/GTKMidiUI"
 IntermediateDirectory  :=../GTKMidiUI/Debug
 OutDir                 := $(IntermediateDirectory)
@@ -13,13 +13,13 @@ CurrentFileName        :=
 CurrentFilePath        :=
 CurrentFileFullPath    :=
 User                   :=Elias Kesh
-Date                   :=05/13/14
+Date                   :=08/10/14
 CodeLitePath           :="/mnt/Personal/home/elias/.codelite"
-LinkerName             :=gcc
-SharedObjectLinkerName :=gcc -shared -fPIC
+LinkerName             :=/usr/bin/gcc
+SharedObjectLinkerName :=/usr/bin/g++ -shared -fPIC
 ObjectSuffix           :=.o
 DependSuffix           :=.o.d
-PreprocessSuffix       :=.o.i
+PreprocessSuffix       :=.i
 DebugSwitch            :=-g 
 IncludeSwitch          :=-I
 LibrarySwitch          :=-l
@@ -31,7 +31,7 @@ OutputFile             :=./LiveMusicApp
 Preprocessors          :=
 ObjectSwitch           :=-o 
 ArchiveOutputSwitch    := 
-PreprocessOnlySwitch   :=-E 
+PreprocessOnlySwitch   :=-E
 ObjectsFileList        :="LiveMusic.txt"
 PCHCompileFlags        :=
 MakeDirCommand         :=mkdir -p
@@ -47,20 +47,20 @@ LibPath                := $(LibraryPathSwitch). $(LibraryPathSwitch)/lib/x86_64-
 ## Common variables
 ## AR, CXX, CC, AS, CXXFLAGS and CFLAGS can be overriden using an environment variables
 ##
-AR       := ar rcus
-CXX      := gcc
-CC       := gcc
+AR       := /usr/bin/ar rcu
+CXX      := /usr/bin/g++ 
+CC       := /usr/bin/gcc
 CXXFLAGS :=  -g -O0 -ggdb  -Wno-cast-qual $(Preprocessors)
 CFLAGS   :=  -pg -g `pkg-config --cflags gtk+-2.0` -ggdb -Wno-cast-qual $(Preprocessors)
 ASFLAGS  := 
-AS       := as
+AS       := /usr/bin/as 
 
 
 ##
 ## User defined environment variables
 ##
 CodeLiteDir:=/usr/share/codelite
-Objects0=$(IntermediateDirectory)/PrefsFile$(ObjectSuffix) $(IntermediateDirectory)/GTKMidiUI$(ObjectSuffix) $(IntermediateDirectory)/AlsaUtils$(ObjectSuffix) $(IntermediateDirectory)/HTML$(ObjectSuffix) $(IntermediateDirectory)/Connections$(ObjectSuffix) $(IntermediateDirectory)/ThirdParty_wmctrl$(ObjectSuffix) $(IntermediateDirectory)/PrefsGui$(ObjectSuffix) $(IntermediateDirectory)/Chorder$(ObjectSuffix) 
+Objects0=$(IntermediateDirectory)/PrefsFile.c$(ObjectSuffix) $(IntermediateDirectory)/GTKMidiUI.c$(ObjectSuffix) $(IntermediateDirectory)/AlsaUtils.c$(ObjectSuffix) $(IntermediateDirectory)/HTML.c$(ObjectSuffix) $(IntermediateDirectory)/Connections.c$(ObjectSuffix) $(IntermediateDirectory)/ThirdParty_wmctrl.c$(ObjectSuffix) $(IntermediateDirectory)/PrefsGui.c$(ObjectSuffix) $(IntermediateDirectory)/Chorder.c$(ObjectSuffix) 
 
 
 
@@ -87,69 +87,69 @@ PreBuild:
 ##
 ## Objects
 ##
-$(IntermediateDirectory)/PrefsFile$(ObjectSuffix): PrefsFile.c $(IntermediateDirectory)/PrefsFile$(DependSuffix)
-	$(CC) $(SourceSwitch) "/mnt/Personal/home/elias/workspace/GTKMidiUI/PrefsFile.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/PrefsFile$(ObjectSuffix) $(IncludePath)
-$(IntermediateDirectory)/PrefsFile$(DependSuffix): PrefsFile.c
-	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/PrefsFile$(ObjectSuffix) -MF$(IntermediateDirectory)/PrefsFile$(DependSuffix) -MM "PrefsFile.c"
+$(IntermediateDirectory)/PrefsFile.c$(ObjectSuffix): PrefsFile.c $(IntermediateDirectory)/PrefsFile.c$(DependSuffix)
+	$(CC) $(SourceSwitch) "/mnt/Personal/home/elias/workspace/GTKMidiUI/PrefsFile.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/PrefsFile.c$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/PrefsFile.c$(DependSuffix): PrefsFile.c
+	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/PrefsFile.c$(ObjectSuffix) -MF$(IntermediateDirectory)/PrefsFile.c$(DependSuffix) -MM "PrefsFile.c"
 
-$(IntermediateDirectory)/PrefsFile$(PreprocessSuffix): PrefsFile.c
-	@$(CC) $(CFLAGS) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/PrefsFile$(PreprocessSuffix) "PrefsFile.c"
+$(IntermediateDirectory)/PrefsFile.c$(PreprocessSuffix): PrefsFile.c
+	@$(CC) $(CFLAGS) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/PrefsFile.c$(PreprocessSuffix) "PrefsFile.c"
 
-$(IntermediateDirectory)/GTKMidiUI$(ObjectSuffix): GTKMidiUI.c $(IntermediateDirectory)/GTKMidiUI$(DependSuffix)
-	$(CC) $(SourceSwitch) "/mnt/Personal/home/elias/workspace/GTKMidiUI/GTKMidiUI.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/GTKMidiUI$(ObjectSuffix) $(IncludePath)
-$(IntermediateDirectory)/GTKMidiUI$(DependSuffix): GTKMidiUI.c
-	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/GTKMidiUI$(ObjectSuffix) -MF$(IntermediateDirectory)/GTKMidiUI$(DependSuffix) -MM "GTKMidiUI.c"
+$(IntermediateDirectory)/GTKMidiUI.c$(ObjectSuffix): GTKMidiUI.c $(IntermediateDirectory)/GTKMidiUI.c$(DependSuffix)
+	$(CC) $(SourceSwitch) "/mnt/Personal/home/elias/workspace/GTKMidiUI/GTKMidiUI.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/GTKMidiUI.c$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/GTKMidiUI.c$(DependSuffix): GTKMidiUI.c
+	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/GTKMidiUI.c$(ObjectSuffix) -MF$(IntermediateDirectory)/GTKMidiUI.c$(DependSuffix) -MM "GTKMidiUI.c"
 
-$(IntermediateDirectory)/GTKMidiUI$(PreprocessSuffix): GTKMidiUI.c
-	@$(CC) $(CFLAGS) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/GTKMidiUI$(PreprocessSuffix) "GTKMidiUI.c"
+$(IntermediateDirectory)/GTKMidiUI.c$(PreprocessSuffix): GTKMidiUI.c
+	@$(CC) $(CFLAGS) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/GTKMidiUI.c$(PreprocessSuffix) "GTKMidiUI.c"
 
-$(IntermediateDirectory)/AlsaUtils$(ObjectSuffix): AlsaUtils.c $(IntermediateDirectory)/AlsaUtils$(DependSuffix)
-	$(CC) $(SourceSwitch) "/mnt/Personal/home/elias/workspace/GTKMidiUI/AlsaUtils.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/AlsaUtils$(ObjectSuffix) $(IncludePath)
-$(IntermediateDirectory)/AlsaUtils$(DependSuffix): AlsaUtils.c
-	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/AlsaUtils$(ObjectSuffix) -MF$(IntermediateDirectory)/AlsaUtils$(DependSuffix) -MM "AlsaUtils.c"
+$(IntermediateDirectory)/AlsaUtils.c$(ObjectSuffix): AlsaUtils.c $(IntermediateDirectory)/AlsaUtils.c$(DependSuffix)
+	$(CC) $(SourceSwitch) "/mnt/Personal/home/elias/workspace/GTKMidiUI/AlsaUtils.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/AlsaUtils.c$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/AlsaUtils.c$(DependSuffix): AlsaUtils.c
+	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/AlsaUtils.c$(ObjectSuffix) -MF$(IntermediateDirectory)/AlsaUtils.c$(DependSuffix) -MM "AlsaUtils.c"
 
-$(IntermediateDirectory)/AlsaUtils$(PreprocessSuffix): AlsaUtils.c
-	@$(CC) $(CFLAGS) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/AlsaUtils$(PreprocessSuffix) "AlsaUtils.c"
+$(IntermediateDirectory)/AlsaUtils.c$(PreprocessSuffix): AlsaUtils.c
+	@$(CC) $(CFLAGS) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/AlsaUtils.c$(PreprocessSuffix) "AlsaUtils.c"
 
-$(IntermediateDirectory)/HTML$(ObjectSuffix): HTML.c $(IntermediateDirectory)/HTML$(DependSuffix)
-	$(CC) $(SourceSwitch) "/mnt/Personal/home/elias/workspace/GTKMidiUI/HTML.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/HTML$(ObjectSuffix) $(IncludePath)
-$(IntermediateDirectory)/HTML$(DependSuffix): HTML.c
-	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/HTML$(ObjectSuffix) -MF$(IntermediateDirectory)/HTML$(DependSuffix) -MM "HTML.c"
+$(IntermediateDirectory)/HTML.c$(ObjectSuffix): HTML.c $(IntermediateDirectory)/HTML.c$(DependSuffix)
+	$(CC) $(SourceSwitch) "/mnt/Personal/home/elias/workspace/GTKMidiUI/HTML.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/HTML.c$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/HTML.c$(DependSuffix): HTML.c
+	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/HTML.c$(ObjectSuffix) -MF$(IntermediateDirectory)/HTML.c$(DependSuffix) -MM "HTML.c"
 
-$(IntermediateDirectory)/HTML$(PreprocessSuffix): HTML.c
-	@$(CC) $(CFLAGS) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/HTML$(PreprocessSuffix) "HTML.c"
+$(IntermediateDirectory)/HTML.c$(PreprocessSuffix): HTML.c
+	@$(CC) $(CFLAGS) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/HTML.c$(PreprocessSuffix) "HTML.c"
 
-$(IntermediateDirectory)/Connections$(ObjectSuffix): Connections.c $(IntermediateDirectory)/Connections$(DependSuffix)
-	$(CC) $(SourceSwitch) "/mnt/Personal/home/elias/workspace/GTKMidiUI/Connections.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Connections$(ObjectSuffix) $(IncludePath)
-$(IntermediateDirectory)/Connections$(DependSuffix): Connections.c
-	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/Connections$(ObjectSuffix) -MF$(IntermediateDirectory)/Connections$(DependSuffix) -MM "Connections.c"
+$(IntermediateDirectory)/Connections.c$(ObjectSuffix): Connections.c $(IntermediateDirectory)/Connections.c$(DependSuffix)
+	$(CC) $(SourceSwitch) "/mnt/Personal/home/elias/workspace/GTKMidiUI/Connections.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Connections.c$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/Connections.c$(DependSuffix): Connections.c
+	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/Connections.c$(ObjectSuffix) -MF$(IntermediateDirectory)/Connections.c$(DependSuffix) -MM "Connections.c"
 
-$(IntermediateDirectory)/Connections$(PreprocessSuffix): Connections.c
-	@$(CC) $(CFLAGS) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Connections$(PreprocessSuffix) "Connections.c"
+$(IntermediateDirectory)/Connections.c$(PreprocessSuffix): Connections.c
+	@$(CC) $(CFLAGS) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Connections.c$(PreprocessSuffix) "Connections.c"
 
-$(IntermediateDirectory)/ThirdParty_wmctrl$(ObjectSuffix): ThirdParty/wmctrl.c $(IntermediateDirectory)/ThirdParty_wmctrl$(DependSuffix)
-	$(CC) $(SourceSwitch) "/mnt/Personal/home/elias/workspace/GTKMidiUI/ThirdParty/wmctrl.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/ThirdParty_wmctrl$(ObjectSuffix) $(IncludePath)
-$(IntermediateDirectory)/ThirdParty_wmctrl$(DependSuffix): ThirdParty/wmctrl.c
-	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/ThirdParty_wmctrl$(ObjectSuffix) -MF$(IntermediateDirectory)/ThirdParty_wmctrl$(DependSuffix) -MM "ThirdParty/wmctrl.c"
+$(IntermediateDirectory)/ThirdParty_wmctrl.c$(ObjectSuffix): ThirdParty/wmctrl.c $(IntermediateDirectory)/ThirdParty_wmctrl.c$(DependSuffix)
+	$(CC) $(SourceSwitch) "/mnt/Personal/home/elias/workspace/GTKMidiUI/ThirdParty/wmctrl.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/ThirdParty_wmctrl.c$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/ThirdParty_wmctrl.c$(DependSuffix): ThirdParty/wmctrl.c
+	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/ThirdParty_wmctrl.c$(ObjectSuffix) -MF$(IntermediateDirectory)/ThirdParty_wmctrl.c$(DependSuffix) -MM "ThirdParty/wmctrl.c"
 
-$(IntermediateDirectory)/ThirdParty_wmctrl$(PreprocessSuffix): ThirdParty/wmctrl.c
-	@$(CC) $(CFLAGS) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/ThirdParty_wmctrl$(PreprocessSuffix) "ThirdParty/wmctrl.c"
+$(IntermediateDirectory)/ThirdParty_wmctrl.c$(PreprocessSuffix): ThirdParty/wmctrl.c
+	@$(CC) $(CFLAGS) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/ThirdParty_wmctrl.c$(PreprocessSuffix) "ThirdParty/wmctrl.c"
 
-$(IntermediateDirectory)/PrefsGui$(ObjectSuffix): PrefsGui.c $(IntermediateDirectory)/PrefsGui$(DependSuffix)
-	$(CC) $(SourceSwitch) "/mnt/Personal/home/elias/workspace/GTKMidiUI/PrefsGui.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/PrefsGui$(ObjectSuffix) $(IncludePath)
-$(IntermediateDirectory)/PrefsGui$(DependSuffix): PrefsGui.c
-	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/PrefsGui$(ObjectSuffix) -MF$(IntermediateDirectory)/PrefsGui$(DependSuffix) -MM "PrefsGui.c"
+$(IntermediateDirectory)/PrefsGui.c$(ObjectSuffix): PrefsGui.c $(IntermediateDirectory)/PrefsGui.c$(DependSuffix)
+	$(CC) $(SourceSwitch) "/mnt/Personal/home/elias/workspace/GTKMidiUI/PrefsGui.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/PrefsGui.c$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/PrefsGui.c$(DependSuffix): PrefsGui.c
+	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/PrefsGui.c$(ObjectSuffix) -MF$(IntermediateDirectory)/PrefsGui.c$(DependSuffix) -MM "PrefsGui.c"
 
-$(IntermediateDirectory)/PrefsGui$(PreprocessSuffix): PrefsGui.c
-	@$(CC) $(CFLAGS) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/PrefsGui$(PreprocessSuffix) "PrefsGui.c"
+$(IntermediateDirectory)/PrefsGui.c$(PreprocessSuffix): PrefsGui.c
+	@$(CC) $(CFLAGS) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/PrefsGui.c$(PreprocessSuffix) "PrefsGui.c"
 
-$(IntermediateDirectory)/Chorder$(ObjectSuffix): Chorder.c $(IntermediateDirectory)/Chorder$(DependSuffix)
-	$(CC) $(SourceSwitch) "/mnt/Personal/home/elias/workspace/GTKMidiUI/Chorder.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Chorder$(ObjectSuffix) $(IncludePath)
-$(IntermediateDirectory)/Chorder$(DependSuffix): Chorder.c
-	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/Chorder$(ObjectSuffix) -MF$(IntermediateDirectory)/Chorder$(DependSuffix) -MM "Chorder.c"
+$(IntermediateDirectory)/Chorder.c$(ObjectSuffix): Chorder.c $(IntermediateDirectory)/Chorder.c$(DependSuffix)
+	$(CC) $(SourceSwitch) "/mnt/Personal/home/elias/workspace/GTKMidiUI/Chorder.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Chorder.c$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/Chorder.c$(DependSuffix): Chorder.c
+	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/Chorder.c$(ObjectSuffix) -MF$(IntermediateDirectory)/Chorder.c$(DependSuffix) -MM "Chorder.c"
 
-$(IntermediateDirectory)/Chorder$(PreprocessSuffix): Chorder.c
-	@$(CC) $(CFLAGS) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Chorder$(PreprocessSuffix) "Chorder.c"
+$(IntermediateDirectory)/Chorder.c$(PreprocessSuffix): Chorder.c
+	@$(CC) $(CFLAGS) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Chorder.c$(PreprocessSuffix) "Chorder.c"
 
 
 -include $(IntermediateDirectory)/*$(DependSuffix)
@@ -157,31 +157,31 @@ $(IntermediateDirectory)/Chorder$(PreprocessSuffix): Chorder.c
 ## Clean
 ##
 clean:
-	$(RM) $(IntermediateDirectory)/PrefsFile$(ObjectSuffix)
-	$(RM) $(IntermediateDirectory)/PrefsFile$(DependSuffix)
-	$(RM) $(IntermediateDirectory)/PrefsFile$(PreprocessSuffix)
-	$(RM) $(IntermediateDirectory)/GTKMidiUI$(ObjectSuffix)
-	$(RM) $(IntermediateDirectory)/GTKMidiUI$(DependSuffix)
-	$(RM) $(IntermediateDirectory)/GTKMidiUI$(PreprocessSuffix)
-	$(RM) $(IntermediateDirectory)/AlsaUtils$(ObjectSuffix)
-	$(RM) $(IntermediateDirectory)/AlsaUtils$(DependSuffix)
-	$(RM) $(IntermediateDirectory)/AlsaUtils$(PreprocessSuffix)
-	$(RM) $(IntermediateDirectory)/HTML$(ObjectSuffix)
-	$(RM) $(IntermediateDirectory)/HTML$(DependSuffix)
-	$(RM) $(IntermediateDirectory)/HTML$(PreprocessSuffix)
-	$(RM) $(IntermediateDirectory)/Connections$(ObjectSuffix)
-	$(RM) $(IntermediateDirectory)/Connections$(DependSuffix)
-	$(RM) $(IntermediateDirectory)/Connections$(PreprocessSuffix)
-	$(RM) $(IntermediateDirectory)/ThirdParty_wmctrl$(ObjectSuffix)
-	$(RM) $(IntermediateDirectory)/ThirdParty_wmctrl$(DependSuffix)
-	$(RM) $(IntermediateDirectory)/ThirdParty_wmctrl$(PreprocessSuffix)
-	$(RM) $(IntermediateDirectory)/PrefsGui$(ObjectSuffix)
-	$(RM) $(IntermediateDirectory)/PrefsGui$(DependSuffix)
-	$(RM) $(IntermediateDirectory)/PrefsGui$(PreprocessSuffix)
-	$(RM) $(IntermediateDirectory)/Chorder$(ObjectSuffix)
-	$(RM) $(IntermediateDirectory)/Chorder$(DependSuffix)
-	$(RM) $(IntermediateDirectory)/Chorder$(PreprocessSuffix)
+	$(RM) $(IntermediateDirectory)/PrefsFile.c$(ObjectSuffix)
+	$(RM) $(IntermediateDirectory)/PrefsFile.c$(DependSuffix)
+	$(RM) $(IntermediateDirectory)/PrefsFile.c$(PreprocessSuffix)
+	$(RM) $(IntermediateDirectory)/GTKMidiUI.c$(ObjectSuffix)
+	$(RM) $(IntermediateDirectory)/GTKMidiUI.c$(DependSuffix)
+	$(RM) $(IntermediateDirectory)/GTKMidiUI.c$(PreprocessSuffix)
+	$(RM) $(IntermediateDirectory)/AlsaUtils.c$(ObjectSuffix)
+	$(RM) $(IntermediateDirectory)/AlsaUtils.c$(DependSuffix)
+	$(RM) $(IntermediateDirectory)/AlsaUtils.c$(PreprocessSuffix)
+	$(RM) $(IntermediateDirectory)/HTML.c$(ObjectSuffix)
+	$(RM) $(IntermediateDirectory)/HTML.c$(DependSuffix)
+	$(RM) $(IntermediateDirectory)/HTML.c$(PreprocessSuffix)
+	$(RM) $(IntermediateDirectory)/Connections.c$(ObjectSuffix)
+	$(RM) $(IntermediateDirectory)/Connections.c$(DependSuffix)
+	$(RM) $(IntermediateDirectory)/Connections.c$(PreprocessSuffix)
+	$(RM) $(IntermediateDirectory)/ThirdParty_wmctrl.c$(ObjectSuffix)
+	$(RM) $(IntermediateDirectory)/ThirdParty_wmctrl.c$(DependSuffix)
+	$(RM) $(IntermediateDirectory)/ThirdParty_wmctrl.c$(PreprocessSuffix)
+	$(RM) $(IntermediateDirectory)/PrefsGui.c$(ObjectSuffix)
+	$(RM) $(IntermediateDirectory)/PrefsGui.c$(DependSuffix)
+	$(RM) $(IntermediateDirectory)/PrefsGui.c$(PreprocessSuffix)
+	$(RM) $(IntermediateDirectory)/Chorder.c$(ObjectSuffix)
+	$(RM) $(IntermediateDirectory)/Chorder.c$(DependSuffix)
+	$(RM) $(IntermediateDirectory)/Chorder.c$(PreprocessSuffix)
 	$(RM) $(OutputFile)
-	$(RM) "../LiveMusic/.build-debug/LiveMusic"
+	$(RM) "../EliasMusic/.build-debug/LiveMusic"
 
 
